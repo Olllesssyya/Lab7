8387d8762c9c3ba710f7f29ff7dd59d4d82db91c
Olesia Fesiuk <O.Fesyuk_FIT_9_22_B_d@knute.edu.ua>
2022-10-09 15:15:37 +0300

    Перший реліз

---
+++ variant8-zavdannya1_lab7.py
@@ -0,0 +1,7 @@
+#Лабароторна 7 варіант 8 завдання1. Посилання на GitHub https://github.com/Olllesssyya/Lab7
+r1=input("Введіть рядок №1: ")
+r2=input("Введіть рядок №2: ")
+for i in r1:
+    if not i in r2:
+        print (i)
+        
---
+++ variant8-zavdannya2_lab7.py
@@ -0,0 +1,14 @@
+#Лабароторна 7 варіант 8 завдання2. Посилання на GitHub https://github.com/Olllesssyya/Lab7
+r1=input("Введіть цілі числа: ")
+r2='x='
+for i in r1:
+    if i==' ':
+        r2=r2+' x='
+    else:
+        r2=r2+i
+print (r2)        
+
+        
+        
+#print ("x=",i)
+        
